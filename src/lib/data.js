22e35ad01f43530d29b109e05003684819d6a279
Laurine-sl <laurine.suel@edu.esiee.fr>
2025-04-12 16:16:51 +0200

    ajout de package + datas

---
+++ src/lib/data.js
@@ -0,0 +1,10 @@
+const api_url =
+  "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/osm-france-food-service/records?select=*&where=type%3D%22restaurant%22%20and%20cuisine!%3D%22null%22%20and%20takeaway!%3D%22null%22&limit=52&refine=meta_name_dep%3A%22Paris%22";
+
+export async function getRestaurants() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results);
+  return data.results;
+}
